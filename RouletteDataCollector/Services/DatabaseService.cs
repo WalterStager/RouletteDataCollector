966ea003b999794d149dbadd9f51f209db2c13f0
Walter Stager <plenta42@gmail.com>
2024-03-15 18:06:12 -0500

    Quick manual examining via config window button

--- RouletteDataCollector/Services/DatabaseService.cs
+++ RouletteDataCollector/Services/DatabaseService.cs
@@ -6,6 +6,7 @@ using Dapper;
 using System.Data.SQLite;
 using RouletteDataCollector.Structs;
 using Dalamud.DrunkenToad.Extensions;
+using System.Collections.Generic;
 
 namespace RouletteDataCollector.Services
 {
@@ -145,6 +146,123 @@ namespace RouletteDataCollector.Services
             }   
         }
 
+        // not for general use
+        private static string getGuidFromList(List<string?> guids, int index)
+        {
+            if (index > guids.Count || index < 0)
+            {
+                return "NULL";
+            }
+            else
+            {
+                return $"'{guids[index]}'";
+            }
+        }
+
+        public void GearsetGearUpdate(string guid, RDCGearset gear, List<string?> materiaSetGuids)
+        {
+            if (plugin.configuration.enableSaveData)
+            {
+                if (materiaSetGuids.Count > 12)
+                {
+                    throw new ArgumentException("materiaSetGuids should have <=12 values");
+                }
+
+                string timestamp = GetTimestamp();
+                plugin.log.Info($"Gearsets gear UPDATE gear {gear}");
+                int rowsAffected = this.sqconn.Execute($@"
+                    UPDATE Gearsets SET
+                        updated = '{timestamp}',
+                        weapon = {gear.items[(int)RDCGearSlot.Weapon]},
+                        offhand = {gear.items[(int)RDCGearSlot.Offhand]},
+                        head = {gear.items[(int)RDCGearSlot.Head]},
+                        body = {gear.items[(int)RDCGearSlot.Body]},
+                        hands = {gear.items[(int)RDCGearSlot.Hands]},
+                        legs = {gear.items[(int)RDCGearSlot.Legs]},
+                        feet = {gear.items[(int)RDCGearSlot.Feet]},
+                        ears = {gear.items[(int)RDCGearSlot.Ears]},
+                        neck = {gear.items[(int)RDCGearSlot.Neck]},
+                        wrists = {gear.items[(int)RDCGearSlot.Wrists]},
+                        ring1 = {gear.items[(int)RDCGearSlot.Ring1]},
+                        ring2 = {gear.items[(int)RDCGearSlot.Ring2]},
+                        materia_weapon = {getGuidFromList(materiaSetGuids, 0)},
+                        materia_offhand = {getGuidFromList(materiaSetGuids, 1)},
+                        materia_head = {getGuidFromList(materiaSetGuids, 2)},
+                        materia_body = {getGuidFromList(materiaSetGuids, 3)},
+                        materia_hands = {getGuidFromList(materiaSetGuids, 4)},
+                        materia_legs = {getGuidFromList(materiaSetGuids, 5)},
+                        materia_feet = {getGuidFromList(materiaSetGuids, 6)},
+                        materia_ears = {getGuidFromList(materiaSetGuids, 7)},
+                        materia_neck = {getGuidFromList(materiaSetGuids, 8)},
+                        materia_wrists = {getGuidFromList(materiaSetGuids, 9)},
+                        materia_ring1 = {getGuidFromList(materiaSetGuids, 10)},
+                        materia_ring2 = {getGuidFromList(materiaSetGuids, 11)},
+                        soulstone = {gear.soulstone}
+                    WHERE id = '{guid}'");
+                if (rowsAffected != 1)
+                {
+                    this.plugin.log.Warning($"Gearsets gear UPDATE affected {rowsAffected} rows instead of 1");
+                }
+            }
+        }
+
+        public void MateriasetInsert(string guid, RDCMateriaset materiaSet)
+        {
+            if (plugin.configuration.enableSaveData)
+            {
+                string timestamp = GetTimestamp();
+                var data = new {
+                    Guid = guid,
+                    Created = timestamp,
+                    Updated = timestamp,
+                    materia0_type = materiaSet.materiaTypes[0],
+                    materia1_type = materiaSet.materiaTypes[1],
+                    materia2_type = materiaSet.materiaTypes[2],
+                    materia3_type = materiaSet.materiaTypes[3],
+                    materia4_type = materiaSet.materiaTypes[4],
+                    materia0_grade = materiaSet.materiaGrades[0],
+                    materia1_grade = materiaSet.materiaGrades[1],
+                    materia2_grade = materiaSet.materiaGrades[2],
+                    materia3_grade = materiaSet.materiaGrades[3],
+                    materia4_grade = materiaSet.materiaGrades[4]};
+                plugin.log.Info($"Materiaset INSERT {data}");
+                int rowsAffected = this.sqconn.Execute(@"
+                    INSERT INTO Materiasets (
+                        id,
+                        created,
+                        updated,
+                        materia0_type,
+                        materia1_type,
+                        materia2_type,
+                        materia3_type,
+                        materia4_type,
+                        materia0_grade,
+                        materia1_grade,
+                        materia2_grade,
+                        materia3_grade,
+                        materia4_grade
+                    ) VALUES (
+                        @Guid,
+                        @Created,
+                        @Updated,
+                        @materia0_type,
+                        @materia1_type,
+                        @materia2_type,
+                        @materia3_type,
+                        @materia4_type,
+                        @materia0_grade,
+                        @materia1_grade,
+                        @materia2_grade,
+                        @materia3_grade,
+                        @materia4_grade
+                    )", data);
+                if (rowsAffected != 1)
+                {
+                    this.plugin.log.Warning($"Materiaset INSERT affected {rowsAffected} rows instead of 1");
+                }
+            }
+        }
+
         private void InitializeDatabase()
         {
             string create_tables = $@"
@@ -173,6 +291,21 @@ namespace RouletteDataCollector.Services
                     lodestone_id        INT,
                     collector           INT
                 );
+                CREATE TABLE IF NOT EXISTS Materiasets (
+                    id                  TEXT NOT NULL PRIMARY KEY,
+                    created             NUMERIC NOT NULL,
+                    updated             NUMERIC NOT NULL,
+                    materia0_type       INT,
+                    materia1_type       INT,
+                    materia2_type       INT,
+                    materia3_type       INT,
+                    materia4_type       INT,
+                    materia0_grade      INT,
+                    materia1_grade      INT,
+                    materia2_grade      INT,
+                    materia3_grade      INT,
+                    materia4_grade      INT
+                );
                 CREATE TABLE IF NOT EXISTS Gearsets (
                     id                  TEXT NOT NULL PRIMARY KEY,
                     created             NUMERIC NOT NULL,
@@ -183,6 +316,7 @@ namespace RouletteDataCollector.Services
                     job                 INT,
                     race                INT,
                     level               INT,
+                    left_early          INT,
                     weapon              INT,
                     offhand             INT,
                     head                INT,
@@ -195,18 +329,19 @@ namespace RouletteDataCollector.Services
                     wrists              INT,
                     ring1               INT,
                     ring2               INT,
-                    materia_weapon      INT,
-                    materia_offhand     INT,
-                    materia_head        INT,
-                    materia_body        INT,
-                    materia_hands       INT,
-                    materia_legs        INT,
-                    materia_feet        INT,
-                    materia_ears        INT,
-                    materia_neck        INT,
-                    materia_wrists      INT,
-                    materia_ring1       INT,
-                    materia_ring2       INT
+                    soulstone           INT,
+                    materia_weapon      TEXT REFERENCES Materiasets (id),
+                    materia_offhand     TEXT REFERENCES Materiasets (id),
+                    materia_head        TEXT REFERENCES Materiasets (id),
+                    materia_body        TEXT REFERENCES Materiasets (id),
+                    materia_hands       TEXT REFERENCES Materiasets (id),
+                    materia_legs        TEXT REFERENCES Materiasets (id),
+                    materia_feet        TEXT REFERENCES Materiasets (id),
+                    materia_ears        TEXT REFERENCES Materiasets (id),
+                    materia_neck        TEXT REFERENCES Materiasets (id),
+                    materia_wrists      TEXT REFERENCES Materiasets (id),
+                    materia_ring1       TEXT REFERENCES Materiasets (id),
+                    materia_ring2       TEXT REFERENCES Materiasets (id)                    
                 );";
 
             this.sqconn.Execute(create_tables);
